2f0a0b5fca6381bcd5c2bdfdb0930db0adba391c
LiliGC <liliana.garmendia.c@gmail.com>
2022-06-06 11:20:14 -0400

    Agregue moderacion a comentarios

--- blog/models.py
+++ blog/models.py
@@ -17,12 +17,15 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
 
     class Meta:
         ordering = ['-created_date']
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
